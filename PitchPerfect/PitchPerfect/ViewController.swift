b7df4444303e0ac7aee1faa9163a9c461dea8671
dgundry <80338563+dgundry@users.noreply.github.com>
2022-09-11 16:36:17 -0400

    Record Button Function

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Devin Gundry on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record Button was Pressed.")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
