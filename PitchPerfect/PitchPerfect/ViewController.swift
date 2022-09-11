856d449432b2ac5e4806f340d5b87b7eebf93590
dgundry <80338563+dgundry@users.noreply.github.com>
2022-09-11 18:03:23 -0400

    Enable/Disable Button Logic

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var stopRecordinButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBAction func recordAudio(_ sender: Any) {
         print("Record Button was Pressed.")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     @IBAction func stopRecording(_ sender: Any) {
         print("Stop Recording Button was Pressed.")
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
